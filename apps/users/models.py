5433af2643213cb4a9dd3053ded4b9aaffa189ed
mikigo <huangmingqiang@uniontech.com>
2022-04-18 01:26:18 +0800

    fix method

--- apps/users/models.py
+++ apps/users/models.py
@@ -27,7 +27,7 @@ class UserProfile(AbstractUser):
     gender = models.CharField(max_length=6, verbose_name="性别", choices=GENDER_CHOICES)
     address = models.CharField(max_length=100, verbose_name="地址", default="")
     mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号码")
-    image = models.ImageField(upload_to="head_image/%Y/%m", default="default.jpg")
+    image = models.ImageField(upload_to="head_image/%Y/%m", default="default.jpg", verbose_name="用户头像")
 
     class Meta:
         verbose_name = "用户信息"
