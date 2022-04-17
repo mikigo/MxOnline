0307f361535f56b98b5b25e5b9e54b02e55addbd
mikigo <huangmingqiang@uniontech.com>
2022-04-17 23:02:55 +0800

    fix method

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+GENDER_CHOICES = (
+    ("male", "男"),
+    ("female", "女"),
+)
+
+# 覆盖django自带的user表
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name="昵称", default="")
+    birthday = models.DateField(max_length=50, verbose_name="生日", null=True, blank=True)
+    gender = models.CharField(max_length=6, verbose_name="性别", choices=GENDER_CHOICES)
+    address = models.CharField(max_length=100, verbose_name="地址", default="")
+    mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号码")
+    image = models.ImageField(upload_to="head_image/%Y/%m", default="default.jpg")
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        if self.nick_name:
+            return self.nick_name
+        return self.username
