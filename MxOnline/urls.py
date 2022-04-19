1ff56cd66760b51cdc29b95182101fce5cceb7cd
mikigo <huangmingqiang@uniontech.com>
2022-04-19 18:32:10 +0800

    fix method

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 import xadmin
 from django.views.generic import TemplateView
+from apps.users.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
     url(r'^ueditor/', include('DjangoUeditor.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="login.html"), name="login"),
+    path('login/', LoginView.as_view(), name="login"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+
+
+class LoginView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, "login.html")
+
+    def post(self, request, *args, **kwargs):
+        user_name = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        # 验证用户名密码, 成功之后会返回一个user对象，否则为None
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
